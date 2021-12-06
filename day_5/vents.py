3a419e1ae454d8f22304e540d03af4d592b2ceab
p0pkern <petermanchristoph@gmail.com>
2021-12-06 09:38:25 -0500

    Fixed diagonal line reading useing Bresenham's algorithm for day_5 part 2.

--- day_5/vents.py
+++ day_5/vents.py
@@ -2,7 +2,9 @@ from pprint import pprint
 
 def vent_file(file_name):
     """
-
+     Opens and reads a text file with plot points as
+     x,y -> x2, y2 and assigns them to a dictionary
+     for processing.
     """
     vent_dict = {}
     with open(file_name) as v_file:
@@ -18,6 +20,9 @@ def vent_file(file_name):
 
 def vent_count(vent_dict):
     """
+     Populates a dictionary with values from a plot for points
+     a to b in a dictionary. If they show up they are marked with
+     the integer 1. If they show up again the integer is incremented.
     """
     grid = {}
     for key, value in vent_dict.items():
@@ -58,65 +63,88 @@ def vent_count(vent_dict):
                       grid[index] += 1
         
         else:
-            index = x
-            index2 = y
-            
-            if x < x2:
-                if y < y2:
-                    while index < x2 or y < y2:
-                        grid_value = str(index)+","+str(index2)
-                        if grid_value not in grid:
-                           grid[grid_value] = 1
-                        else:
-                           grid[grid_value] += 1
+           if abs(y2 - y) < abs(x2 - x):
+              if x > x2:
+                 grid =  plotLineLow(x, y, x2, y2, grid)
+                 grid =  plotLineLow(x2, y2, x, y, grid)
+              else:
+                 grid =  plotLineLow(x, y, x2, y2, grid)
+                 grid =  plotLineLow(x2, y2, x, y, grid)
+           else:
+              if y > y2:
+                 grid =  plotLineHigh(x, y, x2, y2, grid)
+                 grid =  plotLineHigh(x2, y2, x, y, grid)
+              else:
+                 grid = plotLineHigh(x, y, x2, y2, grid)
+                 grid = plotLineHigh(x2, y2, x, y, grid) 
 
-                        if index <= x2:
-                            index += 1
-                        if index2 <= y2:
-                            index2 += 1
-                elif y > y2:
-                    while index < x2 or y > y2:
-                        grid_value = str(index)+","+str(index2)
-                        if grid_value not in grid:
-                           grid[grid_value] = 1
-                        else:
-                           grid[grid_value] += 1
-
-                        if index <= x2:
-                            index += 1
-                        if index2 >= y2:
-                            index2 -= 1
-            elif x > x2:
-                 
-               if y < y2:
-                    while index > x2 or y < y2:
-                        grid_value = str(index)+","+str(index2)
-                        if grid_value not in grid:
-                           grid[grid_value] = 1
-                        else:
-                           grid[grid_value] += 1
-
-                        if index >= x2:
-                            index -= 1
-                        if index2 <= y2:
-                            index2 += 1
-               elif y > y2:
-                     while index > x2 or y > y2:
-                        grid_value = str(index)+","+str(index2)
-                        if grid_value not in grid:
-                           grid[grid_value] = 1
-                        else:
-                           grid[grid_value] += 1
-
-                        if index >= x2:
-                            index -= 1
-                        if index2 >= y2:
-                            index2 -= 1
+                  
     return grid
+
+def plotLineLow(x0, y0, x1, y1, grid):
+    """
+     Plots a diagonal series of points for a 45 degree angle
+     using Bresenhams line algorithm from a low point to a high
+     point
+    """
+    dx = x1 - x0
+    dy = y1 - y0
+    yi = 1
+    if dy < 0:
+        yi = -1
+        dy = -dy
+
+    D = (2 * dy) - dx
+    y = y0
+
+    for x in range(x0, x1 + 1):
+         index = str(x)+","+str(y)
+         if index not in grid:
+             grid[index] = 1
+         else:
+             grid[index] += 1
+
+         if D > 0:
+             y = y + yi
+             D = D - (2 * (dy - dx))
+         D = D + 2*dy
+    
+    return grid
+
+def plotLineHigh(x0, y0, x1, y1, grid):
+   """
+    Plots a diagonal series of points for a 45 degree angle
+    using Bresenhams line algorithm. From a high to low point.
+   """
+    dx = x1 - x0
+    dy = y1 - y0
+    xi = 1
+    if dx < 0:
+        xi = -1
+        dx = -dx
+    D = (2 * dx) - dy
+    x = x0
+
+    for y in range(y0, y1 + 1):
+        index = str(x)+","+str(y)
+        if index not in grid:
+           grid[index] = 1
+        else:
+           grid[index] += 1
+
+        if D > 0:
+           x = x + xi
+           D = D + (2 * (dx - dy))
+        else:
+           D = D + 2 * dx
+
+    return grid
+
+
 
 def total(grid):
     """
-
+    Totals the amount of grid points > 2
     """
     count = 0
     for key, value in grid.items():
@@ -127,7 +155,7 @@ def total(grid):
 
 def vent_number(file_name):
     """
-
+    Find the number of vents that intersect from a text file of data
     """
     vent_dict = vent_file(file_name)
     count = vent_count(vent_dict)
@@ -136,5 +164,5 @@ def vent_number(file_name):
     return totals
 
 if __name__ == "__main__":
-   # print(vent_number("vent_data.txt"))
-    print(vent_number("debug.txt"))    
+    print(vent_number("vent_data.txt"))
+   # print(vent_number("debug.txt"))    
